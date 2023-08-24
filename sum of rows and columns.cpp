8ac39e5bf882a0eb41d00303d2fce1f3b93fbf50
192211500 <123438408+harsha123io@users.noreply.github.com>
2023-08-24 08:09:30 +0530

    Add files via upload

---
+++ sum of rows and columns.cpp
@@ -0,0 +1,32 @@
+#include <stdio.h>
+int main() {
+    int rows, cols;
+    printf("Enter the number of rows: ");
+    scanf("%d", &rows);
+    printf("Enter the number of columns: ");
+    scanf("%d", &cols);
+    int matrix[rows][cols];
+    printf("Enter matrix elements:\n");
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            scanf("%d", &matrix[i][j]);
+        }
+    }
+    printf("Row sums:\n");
+    for (int i = 0; i < rows; i++) {
+        int rowSum = 0;
+        for (int j = 0; j < cols; j++) {
+            rowSum += matrix[i][j];
+        }
+        printf("Row %d: %d\n", i+1, rowSum);
+    }
+    printf("Column sums:\n");
+    for (int j = 0; j < cols; j++) {
+        int colSum = 0;
+        for (int i = 0; i < rows; i++) {
+            colSum += matrix[i][j];
+        }
+        printf("Column %d: %d\n", j+1, colSum);
+    }
+    return 0;
+}
